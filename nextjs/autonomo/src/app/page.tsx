ef26f0928ace3bc71240b4e2cb4f39d5cef109bb
HenriqueArroyo <ana123ca.henrique@gmail.com>
2025-10-20 11:33:54 -0300

    20/10

--- nextjs/autonomo/src/app/page.tsx
+++ nextjs/autonomo/src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import { useState } from "react";
-import styles from "./style/page.css";
+import styles from "./styles/Page.module.css";
 
 interface Anotacao {
   id: string,
